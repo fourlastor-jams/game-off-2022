14312fa1c819d5427195f23946f156ffcffc3cf9
Rui Teixeira <rui@vazteixeira.org>
2022-11-05 11:53:06 +0000

    set collision shape to circle

--- player/Player.cs
+++ player/Player.cs
@@ -4,14 +4,8 @@ using Godot;
 
 public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
     [Export] private float speed = 500f;
     [Export] private bool shouldSlide = false;
-
-    private List<string> directions = new List<string> { };
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
