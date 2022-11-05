cd661bb5fe54c715f550b55c6402693f9651ade7
Rui Teixeira <rui@vazteixeira.org>
2022-11-05 15:03:24 +0000

    remove unused

--- player/Player.cs
+++ player/Player.cs
@@ -8,19 +8,6 @@ public class Player : KinematicBody2D
 
     private bool isRunning = false;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        //
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
-        //
-    }
-
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
