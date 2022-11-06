a9d0f867e114dce7d70f1085de52fdfc00f12719
Daniele Conti <fourlastor@users.noreply.github.com>
2022-11-06 15:46:33 +0000

    Log found object when using action

--- tilemap/Map.cs
+++ tilemap/Map.cs
@@ -1,21 +1,30 @@
 using Godot;
-using System;
 
 public class Map : Node2D
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     private Player player;
+    private TileMap walls;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         player = GetNode<Player>("YSort/Player");
+        walls = GetNode<TileMap>("YSort/Walls");
         player.Connect("OnAction", this, nameof(PlayerAction));
     }
 
     public void PlayerAction(Vector2 direction)
     {
-        GD.Print("Player acting at " + player.Position + ", direction" + direction);
+        var position = walls.WorldToMap(walls.ToLocal(player.GlobalPosition)) + direction;
+        var tileId = walls.GetCellv(position);
+        if (tileId == TileMap.InvalidCell)
+        {
+            GD.Print($"No tile found at {position}");
+            return;
+        }
+        var tileName = walls.TileSet.TileGetName(tileId);
+        GD.Print($"Tile with at position {position}: ID {tileId} - Name {tileName}");
     }
 }
