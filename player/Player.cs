756e9d9cf30411e633c24072486b6e0e3b21ec8f
Rui Teixeira <rui@vazteixeira.org>
2022-11-06 11:04:27 +0000

    add temp player sprite with idle and walk animations

--- player/Player.cs
+++ player/Player.cs
@@ -6,7 +6,16 @@ public class Player : KinematicBody2D
     [Export] private float runningSpeed = 250f;
     [Export] private bool shouldSlide = false;
 
+    private AnimationTree animationTree;
+    private AnimationNodeStateMachinePlayback animationPlayback;
     private bool isRunning = false;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        animationTree = (AnimationTree)GetNode("AnimationTree");
+        animationPlayback = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
+    }
 
     public override void _PhysicsProcess(float delta)
     {
@@ -18,9 +27,19 @@ public class Player : KinematicBody2D
             Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"),
             Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up")
         );
-        var direction = input.Normalized();
-        var movement = (isRunning ? runningSpeed : speed) * direction;
+        var velocity = input.Normalized();
 
+        if (velocity == Vector2.Zero)
+        {
+            animationPlayback.Travel("Idle");
+            return;
+        }
+
+        animationPlayback.Travel("Walk");
+        animationTree.Set("parameters/Walk/blend_position", velocity);
+        animationTree.Set("parameters/Idle/blend_position", velocity);
+
+        var movement = (isRunning ? runningSpeed : speed) * velocity;
         if (shouldSlide)
         {
             MoveAndSlide(movement);
