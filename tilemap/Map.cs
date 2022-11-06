21bd372b22ee0250ea70588a63e39dc9e7448937
Daniele Conti <fourlastor@users.noreply.github.com>
2022-11-06 16:25:40 +0000

    Remove unnecessary comments

--- tilemap/Map.cs
+++ tilemap/Map.cs
@@ -3,11 +3,8 @@ using System;
 
 public class Map : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
     private Player player;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         player = GetNode<Player>("YSort/Player");
