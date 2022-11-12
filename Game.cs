4e361f2efb4eab6b249384c5bc1f0653c594b070
Daniele Conti <fourlastor@users.noreply.github.com>
2022-11-12 16:41:26 +0000

    Start game window to 0.8 of screen size

--- Game.cs
+++ Game.cs
@@ -8,6 +8,12 @@ using JetBrains.Annotations;
     private Inventory inventory;
     private Map map;
 
+    public override void _EnterTree()
+    {
+        var screenSize = OS.GetScreenSize();
+        OS.WindowSize = screenSize * 0.8f;
+    }
+
     public override void _Ready()
     {
         musicPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
