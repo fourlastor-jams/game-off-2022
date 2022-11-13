f84ff835b2d302e2933060759bd213f19cb1df43
SheerSt <8891626+SheerSt@users.noreply.github.com>
2022-11-12 19:32:32 -0600

    inventory tetris works finally

--- inventory/Inventory.cs
+++ inventory/Inventory.cs
@@ -5,6 +5,12 @@ using Godot;
 public class Inventory : Control
 {
     private List<InventorySlot> slots;
+    GridContainer gridContainer;
+
+    // Inventory dragging.
+    public InventorySlot DragFrom;
+    public InventorySlot DragTo;
+    public bool Swapping;
 
     public override void _Ready()
     {
@@ -12,6 +18,14 @@ public class Inventory : Control
         for (var i = 0; i < 16; i++)
         {
             slots.Add(Slot(i));
+        }
+    }
+
+    public override void _Process(float delta)
+    {
+        if (this.DragFrom != null && this.DragTo != null)
+        {
+            this._SwapItems();
         }
     }
 
@@ -23,5 +37,42 @@ public class Inventory : Control
     private InventorySlot Slot(int slot)
     {
         return GetNode<InventorySlot>($"MarginContainer/InventoryGrid/InventorySlot{slot + 1}");
+    }
+
+    private async void _SwapItems()
+    {
+        // Don't call _Process each frame until finished.
+        SetProcess(false);
+        Swapping = true;
+
+        int scale = 24;
+
+        // Get velocity vectors for the 'swap' animation.
+        Vector2 vectorFrom = this.DragTo.RectPosition - (this.DragFrom.RectPosition + this.DragFrom.icon.RectPosition);
+        vectorFrom = vectorFrom / scale;
+        Vector2 vectorTo = this.DragFrom.RectPosition - (this.DragTo.RectPosition + this.DragTo.icon.RectPosition);
+        vectorTo = vectorTo / scale;
+
+        for (int i = 0; i < scale; ++i)
+        {
+            this.DragFrom.icon.RectPosition += vectorFrom;
+            if (this.DragFrom != this.DragTo) this.DragTo.icon.RectPosition += vectorTo;
+
+            // Wait one frame.
+            await ToSignal(GetTree(), "idle_frame");
+        }
+
+        // Actually swap the items at their respective positions now.
+        Item? temp = this.DragFrom.item;
+        this.DragFrom.SetItem(this.DragTo.item);
+        this.DragTo.SetItem(temp);
+
+        // Reset DragFrom and DragTo.
+        this.DragFrom = null;
+        this.DragTo = null;
+
+        // Call _Process each frame from now on.
+        SetProcess(true);
+        Swapping = false;
     }
 }
--- inventory/InventorySlot.cs
+++ inventory/InventorySlot.cs
@@ -3,14 +3,17 @@ using JetBrains.Annotations;
 
 public class InventorySlot : Control
 {
-    [CanBeNull] private Item? item;
+    [CanBeNull] public Item? item { get; set; }
+    public TextureRect icon { get; set; }
 
-    private TextureRect icon;
+    private Inventory Inventory;
+    private Vector2 Offset = new Vector2(1, 2);
 
     public override void _Ready()
     {
         icon = GetNode<TextureRect>("MarginContainer/ItemIcon");
         icon.Connect("gui_input", this, nameof(OnInput));
+        Inventory = (Inventory)FindParent("Inventory");
     }
 
     public bool AddItem(Item newItem)
@@ -20,16 +23,64 @@ public class InventorySlot : Control
             return false;
         }
 
-        item = newItem;
-        icon.Texture = newItem.Texture();
+        this.SetItem(newItem);
+
         return true;
     }
 
+    public void SetItem(Item? newItem)
+    {
+        item = newItem;
+        if (item.HasValue) icon.Texture = newItem.Value.Texture();
+        else icon.Texture = null;  // TODO: I don't think this is a memory leak but I'm not positive.
+        icon.RectPosition = Offset;
+    }
+
+
     public void OnInput([UsedImplicitly] InputEvent @event)
     {
-        if (Input.IsActionJustPressed("item_click") && item != null)
+        if (item == null) return;
+
+        if (Inventory.Swapping) return;
+
+        if (Input.IsActionJustPressed("item_click"))
         {
-            GD.Print($"Click on item {item.Value.Name()}");
+            //GD.Print($"Click on item {item.Value.Name()}");
+            Inventory.DragFrom = this;
         }
-    }
+
+        // Snap the item to the cursor to drag it around.
+        if (Input.IsActionPressed("item_click") && Inventory.DragFrom == this)
+        {
+            Vector2 position = GetViewport().GetMousePosition() - GetGlobalTransformWithCanvas().origin - new Vector2(8, 8);
+            icon.SetPosition(position);
+        }
+
+        if (Inventory.DragFrom == null) return;
+
+        // Snap the item to the cursor to drag it around.
+        if (Input.IsActionJustReleased("item_click"))
+        {
+            // Check if there's another element at this position.
+            GridContainer gridContainer = (GridContainer)GetParent();
+            InventorySlot foundSlot = null;
+            foreach (InventorySlot slot in gridContainer.GetChildren())
+            {
+                GD.Print(slot.GetRect());
+                Vector2 position = RectPosition + icon.RectPosition + new Vector2(8, 8);
+                if (slot.GetRect().HasPoint(position))
+                {
+                    foundSlot = slot;
+                    break;
+                }
+
+            }
+
+            if (foundSlot == null) foundSlot = this;
+
+            GD.Print($"Drag to item {item.Value.Name()}");
+            Inventory.DragTo = foundSlot;
+            Inventory.Swapping = true;
+        }
+    } 
 }
