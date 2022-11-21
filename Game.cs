71a511280e3ce4fc697e2a828fda997553dbcfda
Rui Teixeira <rui@vazteixeira.org>
2022-11-21 09:15:33 +0000

    remove winow resizing as it fits window out of screen

--- Game.cs
+++ Game.cs
@@ -9,12 +9,6 @@ public class Game : Node
     private Inventory inventory;
     private Map map;
 
-    public override void _EnterTree()
-    {
-        var screenSize = OS.GetScreenSize();
-        OS.WindowSize = screenSize * 0.8f;
-    }
-
     public override void _Ready()
     {
         musicPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
