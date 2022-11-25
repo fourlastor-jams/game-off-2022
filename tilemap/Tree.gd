1b028528e1c69b4cfd0b8dcf83dcbb98d13d8268
Daniele Conti <fourlastor@users.noreply.github.com>
2022-11-25 21:35:05 +0300

    Add tree variant to tree scene

---
+++ tilemap/Tree.gd
@@ -0,0 +1,32 @@
+extends StaticBody2D
+tool
+
+
+export(
+    int,
+    "LightGreen",
+    "Green",
+    "DarkGreen",
+    "Purple",
+    "LightPurple",
+    "DarkPurple",
+    "Red",
+    "LightRed",
+    "DarkRed",
+    "Blue",
+    "LightBlue",
+    "DarkBlue",
+    "LightYellow",
+    "Yellow",
+    "DarkYellow"
+    ) var tree_variant = 1;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    $Sprite.frame = tree_variant
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
