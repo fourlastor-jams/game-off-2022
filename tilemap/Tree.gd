f3f4d0008fa55c49902b286059393ffc35035160
Daniele Conti <fourlastor@users.noreply.github.com>
2022-11-26 11:19:25 +0300

    Remove comments

--- tilemap/Tree.gd
+++ tilemap/Tree.gd
@@ -21,12 +21,5 @@ export(
     "DarkYellow"
     ) var variant = 1;
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
     $Sprite.frame = variant
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
